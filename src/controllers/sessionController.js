ec293f81dbaa04d5910eed842e299e98f67f6654
Thiaguim163 <thiagohenriquedr16@gmail.com>
2024-05-13 08:41:21 -0300

    Atualização atividade 13/05/24

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,14 @@
+/*
+index: listagem de sessões
+store(add): criar uma sessão
+show: quando queremos alterar uma ÚNICA sessão
+updade: quando queremos alterar alguma sessão
+destroy(delete): quando queremos deletar uma sessão
+*/
+
+import User from "../models/User";
+import * as Yup from "yup";
+
+class SessionControler {
+  async store(req, res) {}
+}
